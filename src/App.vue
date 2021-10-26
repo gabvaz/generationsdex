b50b7bcb2efebb09755935ec94bac6104e48088d
Gabriel <gabriel.vaz@mesainc.com.br>
2021-10-26 09:40:31 -0300

    setting up files

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@font-face {
+  font-family: "Press Start 2P";
+  src: local("PressStart2P"),
+    url("~@/assets/fonts/PressStart2P-Regular.ttf") format("truetype");
+}
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
